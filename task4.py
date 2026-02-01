ab60276f98ce3551b4c9dbcb6e2d1c98166dd0a0
Chaplin <ann794@yandex.ru>
2026-02-01 18:50:59 +0300

    tasks

--- task4.py
+++ task4.py
@@ -1,16 +1,3 @@
-# Напиши класс EmployeeSalary. Он рассчитывает почасовую заработную плату сотрудников за неделю.
-# С помощью переменной hourly_payment установи почасовой уровень оплаты, равный 400.
-# Проинициализируй атрибуты name, hours, rest_days, email через конструктор.
-# Добавь метод класса get_hours(). Если значение hours неизвестно, метод рассчитывает часы, 
-# исходя из количества выходных — rest_days. Формула для этого случая такая: (7 - rest_days) * 8.
-# Добавь метод класса get_email(). Если значение email неизвестно, метод генерирует его так: f"{name}@email.com".
-# Добавь метод класса set_hourly_payment(). Он меняет значение переменной hourly_payment.
-# Добавь метод расчёта заработной платы salary(). Формула расчёта такая: hours * hourly_payment.
-# У тебя будет три класс-метода. Не забудь, что им всем нужно передать cls в качестве первого аргумента.
-# Метод get_hours() должен вернуть cls: return cls(name, hours, rest_days, email). 
-# Метод get_email — аналогично. Методу set_hourly_payment() ничего возвращать не нужно, 
-# потому что он только меняет значение переменной.
-
 class EmployeeSalary:
     hourly_payment = 400
     
@@ -47,16 +34,3 @@ class EmployeeSalary:
             print("Недостаточно данных для расчета зп, нужны или часы или дни отдыха")
         return salary
     
-    #tests
-# employee_salary1 = EmployeeSalary("Ann", 20, 1, "ann@ya.ru")
-# employee2 = EmployeeSalary.get_email("Ann")
-# print(employee2)
-# employee2.__str__()
-employee3 = EmployeeSalary.get_hours("Anita", None, 2)
-print(f"Employee: {employee3.name}, "
-                f"Hours: {employee3.hours}, "
-                f"Email: {employee3.email if employee3.email else 'Не указан'}")  
-
-employee3.set_hourly_payment(500)
-print(employee3.hourly_payment)
-print(employee3.salary())
